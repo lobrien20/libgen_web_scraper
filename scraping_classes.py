771b24411509717cd751bc01c0973b5f6c553dc6
Leah O'Brien <77538161+lobrien20@users.noreply.github.com>
2022-05-27 11:40:41 +0100

    Update scraping_classes.py

--- scraping_classes.py
+++ scraping_classes.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-#OOP strat time
+
 
 # study object
 
